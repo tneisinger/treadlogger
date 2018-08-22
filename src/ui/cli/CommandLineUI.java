1512fed2222283fcd7718d1ed1b169601c2bf5f9
tjneisi <tjneisi@gmail.com>
2018-08-22 13:09:59 -0700

    Use CommandAdd Command in CommandLineUI

--- src/ui/cli/CommandLineUI.java
+++ src/ui/cli/CommandLineUI.java
@@ -1,20 +1,42 @@
 package ui.cli;
 
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.MissingCommandException;
 
 public class CommandLineUI {
 
-    private ArgsObject argsObject;
+    private CommandAdd commandAdd;
+    private JCommander jCommander;
 
     public CommandLineUI(String[] args) {
-        this.argsObject = new ArgsObject();
-        JCommander.newBuilder().addObject(argsObject).build().parse(args);
+        this.commandAdd = new CommandAdd();
+        this.jCommander = JCommander.newBuilder()
+                .addCommand("add", this.commandAdd)
+                .build();
+
+        // If an unrecognized command is parsed, handle the exception by informing the user
+        try {
+            this.jCommander.parse(args);
+        } catch (MissingCommandException e) {
+            System.out.println("An unrecognized treadlogger command was given. The valid commands are: ");
+            for (String command : this.jCommander.getCommands().keySet()) {
+                System.out.println("  " + command);
+            }
+        }
     }
 
     public void run() {
-        System.out.println("Here are the parameters: ");
-        for (String parameter : this.argsObject.getParameters()) {
-            System.out.println("  " + parameter);
+        // If no valid command was parsed, take no further action.
+        if (this.jCommander.getParsedCommand() == null) {
+            return;
         }
+
+        System.out.println("The parsed command was: " + this.jCommander.getParsedCommand());
+
+        String time = this.commandAdd.getTimeAndDistance().get(0);
+        String distance = this.commandAdd.getTimeAndDistance().get(1);
+        System.out.println("The time was: " + time);
+        System.out.println("The distance was: " + distance);
+
     }
 }
