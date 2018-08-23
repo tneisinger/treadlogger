df61bfed0a9c4c670bcdc87314ff859c69325ccb
tjneisi <tjneisi@gmail.com>
2018-08-22 18:25:55 -0700

    Define ElapsedTimeFormatException

---
+++ src/logic/ElapsedTimeFormatException.java
@@ -0,0 +1,8 @@
+package logic;
+
+public class ElapsedTimeFormatException extends Exception {
+    public ElapsedTimeFormatException() { super(); }
+    public ElapsedTimeFormatException(String message) { super(message); }
+    public ElapsedTimeFormatException(String message, Throwable cause) { super(message, cause); }
+    public ElapsedTimeFormatException(Throwable cause) { super(cause); }
+}
