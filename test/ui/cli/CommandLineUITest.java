a4f9f8e88293dee999276d0029d97d2aa547c65f
tjneisi <tjneisi@gmail.com>
2018-08-23 18:22:35 -0700

    Add one test for the CommandLineUI constructor

--- test/ui/cli/CommandLineUITest.java
+++ test/ui/cli/CommandLineUITest.java
@@ -1,12 +1,34 @@
 package ui.cli;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommandLineUITest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
 
     @Test
-    void testRun() {
+    void testConstructorNonExistentCommand() {
+        String[] args = new String[]{"NonExistentCommand", "arg1", "arg2"};
+        CommandLineUI ui = new CommandLineUI(args);
+        assertTrue(this.outContent.toString().startsWith("An unrecognized treadlogger command was given."));
     }
 }
