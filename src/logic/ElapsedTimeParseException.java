5ef7ef5ed51bccfee5afb8284c1810f37adf4200
tjneisi <tjneisi@gmail.com>
2018-08-24 20:17:49 -0700

    Rename Exception to ElapsedTimeParseException

---
+++ src/logic/ElapsedTimeParseException.java
@@ -0,0 +1,8 @@
+package logic;
+
+public class ElapsedTimeParseException extends Exception {
+    public ElapsedTimeParseException() { super(); }
+    public ElapsedTimeParseException(String message) { super(message); }
+    public ElapsedTimeParseException(String message, Throwable cause) { super(message, cause); }
+    public ElapsedTimeParseException(Throwable cause) { super(cause); }
+}
