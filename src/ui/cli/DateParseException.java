91345f4d4b97e507efbcff5a059c7713cf2e08fc
tjneisi <tjneisi@gmail.com>
2018-08-25 11:32:30 -0700

    Remove DateParseException and TimeParseException

--- src/ui/cli/DateParseException.java
+++
@@ -1,8 +0,0 @@
-package ui.cli;
-
-public class DateParseException extends Exception {
-    public DateParseException() { super(); }
-    public DateParseException(String message) { super(message); }
-    public DateParseException(String message, Throwable cause) { super(message, cause); }
-    public DateParseException(Throwable cause) { super(cause); }
-}
--- src/ui/cli/TimeParseException.java
+++
@@ -1,8 +0,0 @@
-package ui.cli;
-
-public class TimeParseException extends Exception {
-    public TimeParseException() { super(); }
-    public TimeParseException(String message) { super(message); }
-    public TimeParseException(String message, Throwable cause) { super(message, cause); }
-    public TimeParseException(Throwable cause) { super(cause); }
-}
