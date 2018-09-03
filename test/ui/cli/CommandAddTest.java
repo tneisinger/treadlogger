6fe33cfcdd61a5df4bc619bfd9cfedb951bff99e
tjneisi <tjneisi@gmail.com>
2018-09-03 14:59:09 -0700

    Improve BeforeEach method and add AfterEach method

--- test/ui/cli/CommandAddTest.java
+++ test/ui/cli/CommandAddTest.java
@@ -2,23 +2,34 @@ package ui.cli;
 
 import com.beust.jcommander.JCommander;
 import logic.db.SqliteDB;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommandAddTest {
 
+    private static final String TEST_DB_PATH = "test_treadlogger.sqlite3";
+    private SqliteDB db;
     private CommandAdd commandAdd;
     private JCommander jCommander;
 
     @BeforeEach
     void setUp() {
-        SqliteDB db = new SqliteDB();
+        this.db = new SqliteDB(TEST_DB_PATH);
         this.commandAdd = new CommandAdd(db);
         this.jCommander = JCommander.newBuilder()
                 .addCommand("add", this.commandAdd)
                 .build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        File dbFile = new File(TEST_DB_PATH);
+        assertTrue(dbFile.delete());
     }
 
     @Test
